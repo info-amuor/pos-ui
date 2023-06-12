0e042e70f40814ca148ea354953c8d58fda16815
ozen-hub <info.ozenhub@gmail.com>
2023-06-12 21:17:29 +0530

    4

--- js/customer.js
+++ js/customer.js
@@ -6,7 +6,7 @@ const createOrUpdateCustomer = () => {
             address: $('#address').val(),
             salary: parseFloat(salary)
         }
-
+        document.getElementById('loader').style.display='flex';
         $.ajax({
             url: 'http://localhost:8001/api/v1/customers',
             data: JSON.stringify(customerData),
@@ -14,9 +14,13 @@ const createOrUpdateCustomer = () => {
             method: 'POST',
             success: (response) => {
                 console.log(response);
+                toastr.success('Successfully Created.')
+                document.getElementById('loader').style.display='none';
             },
             error: (error) => {
+                document.getElementById('loader').style.display='none';
                 console.error('This is an Error', error)
+                toastr.error('Error.')
             }
         });
 
@@ -26,4 +30,21 @@ const createOrUpdateCustomer = () => {
     }
 
 
+}
+const loadData=()=>{
+    document.getElementById('loader').style.display='flex';
+    $.ajax({
+        url: 'http://localhost:8001/api/v1/customers/list?page=0&size=10&searchText=',
+        contentType: 'application/json',
+        method: 'GET',
+        success: (response) => {
+            console.log(response);
+            document.getElementById('loader').style.display='none';
+        },
+        error: (error) => {
+            document.getElementById('loader').style.display='none';
+            console.error('This is an Error', error)
+            toastr.error('Error.')
+        }
+    });
 }
